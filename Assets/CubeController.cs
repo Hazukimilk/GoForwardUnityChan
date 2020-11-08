74e16b1ff7b0f30025feb8029421027d7ff03000
Hazukimilk <60565426+Hazukimilk@users.noreply.github.com>
2020-11-08 19:25:49 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    private float speed = -12;
+
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/CubePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlayer : MonoBehaviour
+{
+
+
+    Rigidbody2D rigid2D;
+    private AudioSource audioSource;
+    private bool isInAir = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        if (rigid2D.velocity.y > 0 && isInAir)
+        {
+            audioSource.Play();
+            isInAir = false;
+        }
+    }
+}
+
